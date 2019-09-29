2e11271a45620558a2532d447a7682cc6e2f36fd
Jason Robitaille <robitaille88@gmail.com>
2019-09-29 16:50:30 -0700

    Added basic socket support libraries and socket server setup.

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 /* eslint-env node */
 
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
+
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server)
 const port = 80;
 
-app.use('/', express.static(path.join(__dirname, 'dist')))
+app.use('/', express.static(path.join(__dirname, 'dist')));
 
-app.listen(port, () => console.log(`PimiiboInteractive server listening on port ${port}!`))
+io.on('connection', function(socket){
+	console.log('a user connected', socket.id);
+});
+
+server.listen(port, () => console.log(`PimiiboInteractive server listening on port ${port}!`))
