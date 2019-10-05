043b701ea78a337e30d06cfc5cf27f54d040a82f
Jason Robitaille <robitaille88@gmail.com>
2019-10-05 11:45:52 -0700

    Automatically can all bin files and map to id values for usage, rebuilding map as needed.

---
+++ server/bin-files.js
@@ -0,0 +1,43 @@
+/* eslint-env node */
+
+const fs = require('fs');
+const path = require('path');
+const glob = require('glob');
+
+const dir = path.join(__dirname, '..', 'amiibo');
+const binFiles = new Map();
+
+function scan() {
+	binFiles.clear();
+	try {
+		const found = glob.sync('**/*.bin', {
+			cwd: dir,
+			ignore: 'key_retail.bin',
+			absolute: true,
+			nodir: true
+		});
+		found.forEach(f => {
+			try {
+				const fd = fs.openSync(f, 'r', 0o666);
+				const buf = Buffer.alloc(8)
+				fs.readSync(fd, buf, 0, 8, 84);
+				fs.closeSync(fd);
+				binFiles.set(buf.toString('hex'), f);
+			} catch(e) {
+				// fs error, do nothing
+			}
+		});
+	} catch(e) {
+		// glob error, do nothing
+	}
+}
+
+function lookup(key) {
+	return binFiles.get(key);
+}
+
+module.exports = {
+	dir,
+	scan,
+	lookup
+};
--- server/index.js
+++ server/index.js
@@ -4,7 +4,22 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
+const chokidar = require('chokidar');
+const binFiles = require('./bin-files');
 
+console.log('Initializing local Amiibo file list...');
+binFiles.scan();
+console.log('\tDONE');
+
+chokidar.watch(binFiles.dir, {ignoreInitial: true}).on('all', (ev) => {
+	if (['add', 'change', 'unlink'].includes(ev)) {
+		console.log('Change detected, rescanning local Amiibo file list', ev);
+		binFiles.scan();
+		console.log('\tDONE');
+	}
+});
+
+console.log('Starting server...');
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server)
